73823c9847900fe8acaa1ff3e68f096891d69559
Amar J <69858931+amar-jagdale@users.noreply.github.com>
2021-09-07 16:09:01 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,56 @@
 ﻿using System;
-using ClassLibrary1;
-namespace CRUD_Console
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace Binary_Serialization
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("=================== CRUD__IMPLEMENTATION USING SQL =======================");
-            Class1 obj = new Class1();
-            obj.Addemployee();
-            obj.DisplayEmployee();
-            //obj.DisplayEmployee();
-            //obj.ExecuteData();
-            //obj.ExecuteCommand();
+            Console.WriteLine("============== Serialization and Deserialization =============");
+            Person objperson = new Person() { FirstName = "Amar", LastName = "Jagdale" };
+            string filPath = "data.save";
 
         }
     }
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Dataserializer
+    {
+        //serialization
+        public void BinarySerialize(object data, string filePath)
+        {
+            // check file is exist or not if does exist then we need to delete file first then create new
+            FileStream fileStream;
+            BinaryFormatter bf = new BinaryFormatter();
+            if (File.Exists(filePath)) File.Delete(filePath);
+            fileStream = File.Create(filePath);
+            //Create serialize the object
+
+            bf.Serialize(fileStream, data);
+            fileStream.Close();
+        }
+
+
+        //Deserialization
+        public object BinaryDeserialize(string filePath)
+        {
+            object obj = null;
+            FileStream fileStream;
+            BinaryFormatter bf = new BinaryFormatter();
+            if (File.Exists(filePath))
+            {
+                fileStream = File.OpenRead(filePath);
+                obj =bf.Deserialize(fileStream);
+                fileStream.Close();
+            }
+        }
+    }
 }
+
+
