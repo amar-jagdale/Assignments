9417bf6b8c4590c121cf746928f5329f1693dd4b
hp <hp@DESKTOP-TL0FRLL>
2021-09-08 01:26:52 +0530

    Async and Await Thread

---
+++ Async_Await_Implement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace Async_Await_Implement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============== Async and Await =================");
+            Method1();
+            Method2();
+        }
+
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i <20;  i++)
+                {
+                    Console.WriteLine("Method 1");
+                    Task.Delay(200).Wait();
+                }
+            });
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(" Method 2");
+                // Do something
+                Task.Delay(200).Wait();
+            }
+        }
+    }
+}
