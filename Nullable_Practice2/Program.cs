c4869904d51d5a3d184dd19aa53ef11a7a75cef3
hp <hp@DESKTOP-TL0FRLL>
2021-09-08 01:58:13 +0530

    Nullable Practice

---
+++ Nullable_Practice2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Nullable_Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============ Nullable ========");
+            Myclass mycls = new Myclass();
+            if (mycls.i == null)
+            {
+                Console.WriteLine("Null");
+            }
+            else
+            {
+                Console.WriteLine("Not Null");
+            }
+        }
+    }
+
+   public class Myclass
+    {
+        public Nullable<int> i = null;
+    }
+}
+
+
