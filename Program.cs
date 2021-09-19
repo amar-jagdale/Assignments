f36e10b9c7fc899865b4354e65bc98f7731e267e
Amar J <69858931+amar-jagdale@users.noreply.github.com>
2021-09-19 17:43:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.NET_Console_SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------- Implementaion of ADO.NET SQl server to Console Application Data Retriving -----------");
+            string cs = "data source=DESKTOP-TL0FRLL\\SQLEXPRESS;Initial Catalog=College;integrated security=True";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Student;", con);
+            //SqlCommand cmd = new SqlCommand("select * from Student where branch='CSE';", con);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                string id = dr["id"].ToString();
+                string Name = dr["Name"].ToString();
+                string Branch = dr["Branch"].ToString();
+
+                Console.WriteLine(id+"  "+Name+"  "+Branch);
+            }
+            dr.Close();
+            con.Close();
+            Console.ReadLine();
+        }
+    }
+}
